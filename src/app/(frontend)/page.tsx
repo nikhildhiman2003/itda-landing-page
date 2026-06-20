9a9588872032430d4cb3f9b318763cc0cd6de53d
Nikhil Dhiman <nikhil2003dhiman@gmail.com>
2026-06-20 15:17:24 +0530

    feat: add FeaturedServices, About, and DepartmentServices components to HomePage

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Navbar from '../../components/navbar'
 import Hero from '../../components/hero'
+import FeaturedServices from '../../components/feature-services'
+import About from '../../components/about-section'
+import DepartmentServices from '../../components/department-services'
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-white flex flex-col">
@@ -8,6 +11,9 @@ export default function HomePage() {
       <Navbar />
       <main className="flex-grow" id="main-content">
         <Hero />
-      </main>
+        <FeaturedServices />
+        <About />  
+        <DepartmentServices />    
+        </main>
     </div>
   )}
