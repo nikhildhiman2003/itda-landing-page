d3d8a4f80d0ccf5bec5c78c4cf98a962c69de2bc
Nikhil Dhiman <nikhil2003dhiman@gmail.com>
2026-06-24 16:17:58 +0530

    feat: add Aadhar global configuration with fields for service management

---
+++ src/payload/globals/aadhar.ts
@@ -0,0 +1,50 @@
+import { GlobalConfig } from 'payload'
+import { METADATA_FIELD } from '../utils'
+
+export const Aadhar: GlobalConfig = {
+  slug: 'aadhar',
+  admin: {
+    description: 'Manage Aadhar Services Info Portal Settings',
+  },
+  access: {
+    read: () => true, 
+  },
+  fields: [
+    METADATA_FIELD,
+    {
+      type: 'upload',
+      relationTo: 'media',
+      name: 'heroImage',
+      required: true,
+      label: 'Hero Image Asset',
+    },
+    {
+      type: 'text',
+      name: 'heading',
+      label: 'Heading',
+      required: true,
+      localized: true,
+    },
+    {
+      type: 'text',
+      name: 'about',
+      label: 'About Section Text',
+      required: true,
+      localized: true,
+    },
+    {
+      type: 'text',
+      name: 'stats',
+      label: 'Statistics Main Title',
+      required: true,
+      localized: true,
+    },
+    {
+      type: 'richText',
+      name: 'statPoints',
+      label: 'Statistical Bullet Points',
+      required: true,
+      localized: true,
+    },
+  ],
+}
