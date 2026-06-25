c8bce74bcd4bc8d8c8e3e2696a6d835593c6ceea
Nikhil Dhiman <nikhil2003dhiman@gmail.com>
2026-06-25 11:13:35 +0530

    feat: remove Navbar and Footer components from HomePage

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,24 +1,20 @@
 import React from 'react'
-import Navbar from '../../components/navbar'
 import Hero from '../../components/hero'
 import FeaturedServices from '../../components/feature-services'
 import About from '../../components/about-section'
 import DepartmentServices from '../../components/department-services'
 import Partners from '../../components/partners'
-import Footer from '../../components/footer'
+
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-white flex flex-col">
-    
-      <Navbar />
       <main className="flex-grow" id="main-content">
         <Hero />
         <FeaturedServices />
         <About />  
         <DepartmentServices />  
         <Partners />
-        <Footer />
         </main>
     </div>
   )}
