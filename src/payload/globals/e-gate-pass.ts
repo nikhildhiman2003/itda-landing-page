3266425bedaa5b16eaff5a4ebd4be2eef46f493b
Nikhil Dhiman <nikhil2003dhiman@gmail.com>
2026-06-27 21:23:28 +0530

    feat: add E-Gate Pass global configuration with fields for media and text

---
+++ src/payload/globals/e-gate-pass.ts
@@ -0,0 +1,49 @@
+import { GlobalConfig } from 'payload' 
+import { METADATA_FIELD } from '../utils'
+
+export const EGatePass = {
+  slug: 'e-gate-pass',
+  admin: {
+    description: 'E-Gate Pass',
+  },
+  access: {
+    read: () => true, 
+  },
+  fields: [
+    METADATA_FIELD,
+    {
+      type: 'upload',
+      relationTo: 'media',
+      name: 'heroImage',
+      label: 'Hero Image',
+      required: true,
+    },
+    {
+      type: 'text',
+      name: 'heading',
+      label: 'Heading',
+      required: true,
+      localized: true,
+    },
+    {
+      type: 'text',
+      name: 'about',
+      label: 'About',
+      required: true,
+      localized: true,
+    },
+    {
+      type: 'richText',
+      name: 'visitLink',
+      label: 'Visit Link Rich Text',
+      required: true,
+      localized: true,
+    },
+    {
+      type: 'text',
+      name: 'link',
+      label: 'Redirect Link / URL',
+      required: true,
+    },
+  ],
+} satisfies GlobalConfig
