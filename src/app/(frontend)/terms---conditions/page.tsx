d0b4213eeaf16b5517e07461b84d90dbf328efcb
Nikhil Dhiman <nikhil2003dhiman@gmail.com>
2026-06-30 12:46:56 +0530

    feat: add Privacy Policy page with terms and conditions content

---
+++ src/app/(frontend)/terms---conditions/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Privacy() {
+  return (
+    <section className="w-full py-10 md:py-14 lg:py-16">
+      <div className="mx-auto max-w-7xl px-5 sm:px-8 md:px-12 lg:px-20">
+        <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-black leading-tight">
+          Terms & Conditions
+        </h1>
+
+        <p className="mt-6 text-base sm:text-lg leading-8 text-black">
+         This website is designed, developed and hosted by ITDA Uttarakhand. Contents on this website is published, managed and maintained by Information Technology Development Agency.
+
+         Though all efforts have been made to ensure the accuracy and currency of the content on this website, the same should not be construed as a statement of law or used for any legal purposes. In case of any ambiguity or doubts, users are advised to verify / check with the Department(s) and / or other source(s), and to obtain appropriate professional advice.
+
+        Under no circumstances will this Department be liable for any expense, loss or damage including, without limitation, indirect or consequential loss or damage, or any expense, loss or damage whatsoever arising from use, or loss of use, of data, arising out of or in connection with the use of this website.
+
+        These terms and conditions shall be governed by and constructed in accordance with the Indian Laws. Any dispute arising under these terms and conditions shall be subject to the jurisdiction of the courts of India.
+        </p>
+      </div>
+    </section>
+  )
+}
